f424f87cfc0a4c4da23eba9ee0d7cf60f1c01436
Eric Hodel <drbrain@segment7.net>
2012-01-31 14:57:57 -0800

    Set release date

--- lib/net/http/persistent.rb
+++ lib/net/http/persistent.rb
@@ -149,7 +149,7 @@ class Net::HTTP::Persistent
   ##
   # The version of Net::HTTP::Persistent you are using
 
-  VERSION = '2.3.3'
+  VERSION = '2.4'
 
   ##
   # Error class for errors raised by Net::HTTP::Persistent.  Various
