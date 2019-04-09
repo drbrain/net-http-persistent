drbrain/net-http-persistent

commit 5bae22e8a3626ab3b14743c00796d4f176580ad8

Use a connection pool to manage connections
We now use the connection_pool gem instead of per-thread connection
stores.  This improves reuse of connections across threads and reduces
the possibility of using all the file descriptors in the process.

This removes the Thread argument for #shutdown (which now shuts down all
connections and may be dangerous, needs verification).

This removes #shutdown_in_all_threads as we no longer have per-thread
storage.

This adds the Net::HTTP::Persistent::Connection class to manage
connection metadata.

This makes Net::HTTP::Persistent's tests a little easier to read as you
no longer have to worry about the per-thread data stores.

lib/net/http/persistent.rb
@@ -7,6 +7,7 @@ end if RUBY_VERSION < '1.9' # but only for 1.8
 require 'net/http/faster'
 require 'uri'
 require 'cgi' # for escaping
+require 'connection_pool'
 
 begin
   require 'net/http/pipeline'
@@ -252,31 +253,31 @@ class Net::HTTP::Persistent
 
     http = new 'net-http-persistent detect_idle_timeout'
 
-    connection = http.connection_for uri
+    http.connection_for uri do |connection|
+      sleep_time = 0
 
-    sleep_time = 0
+      http = connection.http
 
-    loop do
-      response = connection.request req
+      loop do
+        response = http.request req
 
-      $stderr.puts "HEAD #{uri} => #{response.code}" if $DEBUG
+        $stderr.puts "HEAD #{uri} => #{response.code}" if $DEBUG
 
-      unless Net::HTTPOK === response then
-        raise Error, "bad response code #{response.code} detecting idle timeout"
+        unless Net::HTTPOK === response then
+          raise Error, "bad response code #{response.code} detecting idle timeout"
+        end
+
+        break if sleep_time >= max
+
+        sleep_time += 1
+
+        $stderr.puts "sleeping #{sleep_time}" if $DEBUG
+        sleep sleep_time
       end
-
-      break if sleep_time >= max
-
-      sleep_time += 1
-
-      $stderr.puts "sleeping #{sleep_time}" if $DEBUG
-      sleep sleep_time
     end
   rescue
     # ignore StandardErrors, we've probably found the idle timeout.
   ensure
-    http.shutdown
-
     return sleep_time unless $!
   end
 
@@ -398,14 +399,14 @@ class Net::HTTP::Persistent
   attr_reader :no_proxy
 
   ##
+  # Test-only accessor for the connection pool
+
+  attr_reader :pool # :nodoc:
+
+  ##
   # Seconds to wait until reading one block.  See Net::HTTP#read_timeout
 
   attr_accessor :read_timeout
-
-  ##
-  # Where this instance's request counts live in the thread local variables
-
-  attr_reader :request_key # :nodoc:
 
   ##
   # By default SSL sessions are reused to avoid extra SSL handshakes.  Set
@@ -519,6 +520,7 @@ class Net::HTTP::Persistent
     @idle_timeout     = 5
     @max_requests     = nil
     @socket_options   = []
+    @ssl_generation   = 0 # incremented when SSL session variables change
 
     @socket_options << [Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1] if
       Socket.const_defined? :TCP_NODELAY
@@ -526,8 +528,12 @@ class Net::HTTP::Persistent
     key = ['net_http_persistent', name].compact
     @generation_key     = [key, 'generations'    ].join('_').intern
     @ssl_generation_key = [key, 'ssl_generations'].join('_').intern
-    @request_key        = [key, 'requests'       ].join('_').intern
     @timeout_key        = [key, 'timeouts'       ].join('_').intern
+
+    pool_size = Process.getrlimit(Process::RLIMIT_NOFILE).first / 4
+    @pool     = Net::HTTP::Persistent::Pool.new size: pool_size do |http_args|
+      Net::HTTP::Persistent::Connection.new http_class, http_args, @ssl_generation
+    end
 
     @certificate        = nil
     @ca_file            = nil
@@ -542,7 +548,6 @@ class Net::HTTP::Persistent
     @cert_store         = nil
 
     @generation         = 0 # incremented when proxy URI changes
-    @ssl_generation     = 0 # incremented when SSL session variables change
 
     if HAVE_OPENSSL then
       @verify_mode        = OpenSSL::SSL::VERIFY_PEER
@@ -633,64 +638,45 @@ class Net::HTTP::Persistent
   # Creates a new connection for +uri+
 
   def connection_for uri
-    Thread.current[@generation_key]     ||= Hash.new { |h,k| h[k] = {} }
-    Thread.current[@ssl_generation_key] ||= Hash.new { |h,k| h[k] = {} }
-    Thread.current[@request_key]        ||= Hash.new 0
     Thread.current[@timeout_key]        ||= Hash.new EPOCH
 
     use_ssl = uri.scheme.downcase == 'https'
 
-    if use_ssl then
-      raise Net::HTTP::Persistent::Error, 'OpenSSL is not available' unless
-        HAVE_OPENSSL
-
-      ssl_generation = @ssl_generation
-
-      ssl_cleanup ssl_generation
-
-      connections = Thread.current[@ssl_generation_key][ssl_generation]
-    else
-      generation = @generation
-
-      cleanup generation
-
-      connections = Thread.current[@generation_key][generation]
-    end
-
     net_http_args = [uri.host, uri.port]
-    connection_id = net_http_args.join ':'
 
-    if @proxy_uri and not proxy_bypass? uri.host, uri.port then
-      connection_id << @proxy_connection_id
-      net_http_args.concat @proxy_args
+    net_http_args.concat @proxy_args if
+      @proxy_uri and not proxy_bypass? uri.host, uri.port
+
+    connection = @pool.checkout net_http_args
+
+    http = connection.http
+
+    connection.ressl @ssl_generation if
+      connection.ssl_generation != @ssl_generation
+
+    if not http.started? then
+      ssl   http if use_ssl
+      start http
+    elsif expired? connection then
+      reset connection
     end
 
-    connection = connections[connection_id]
+    http.read_timeout = @read_timeout if @read_timeout
+    http.keep_alive_timeout = @idle_timeout if @idle_timeout && http.respond_to?(:keep_alive_timeout=)
 
-    unless connection = connections[connection_id] then
-      connections[connection_id] = http_class.new(*net_http_args)
-      connection = connections[connection_id]
-      ssl connection if use_ssl
-    else
-      reset connection if expired? connection
-    end
-
-    start connection unless connection.started?
-
-    connection.read_timeout = @read_timeout if @read_timeout
-    connection.keep_alive_timeout = @idle_timeout if @idle_timeout && connection.respond_to?(:keep_alive_timeout=)
-
-    connection
+    return yield connection
   rescue Errno::ECONNREFUSED
-    address = connection.proxy_address || connection.address
-    port    = connection.proxy_port    || connection.port
+    address = http.proxy_address || http.address
+    port    = http.proxy_port    || http.port
 
     raise Error, "connection refused: #{address}:#{port}"
   rescue Errno::EHOSTDOWN
-    address = connection.proxy_address || connection.address
-    port    = connection.proxy_port    || connection.port
+    address = http.proxy_address || http.address
+    port    = http.proxy_port    || http.port
 
     raise Error, "host down: #{address}:#{port}"
+  ensure
+    @pool.checkin net_http_args
   end
 
   ##
@@ -698,12 +684,11 @@ class Net::HTTP::Persistent
   # this connection
 
   def error_message connection
-    requests = Thread.current[@request_key][connection.object_id] - 1 # fixup
-    last_use = Thread.current[@timeout_key][connection.object_id]
+    connection.requests -= 1 # fixup
 
-    age = Time.now - last_use
+    age = Time.now - connection.last_use
 
-    "after #{requests} requests on #{connection.object_id}, " \
+    "after #{connection.requests} requests on #{connection.http.object_id}, " \
       "last used #{age} seconds ago"
   end
 
@@ -727,26 +712,23 @@ class Net::HTTP::Persistent
   # maximum request count, false otherwise.
 
   def expired? connection
-    requests = Thread.current[@request_key][connection.object_id]
-    return true  if     @max_requests && requests >= @max_requests
+    return true  if     @max_requests && connection.requests >= @max_requests
     return false unless @idle_timeout
     return true  if     @idle_timeout.zero?
 
-    last_used = Thread.current[@timeout_key][connection.object_id]
-
-    Time.now - last_used > @idle_timeout
+    Time.now - connection.last_use > @idle_timeout
   end
 
   ##
   # Starts the Net::HTTP +connection+
 
-  def start connection
-    connection.set_debug_output @debug_output if @debug_output
-    connection.open_timeout = @open_timeout if @open_timeout
+  def start http
+    http.set_debug_output @debug_output if @debug_output
+    http.open_timeout = @open_timeout if @open_timeout
 
-    connection.start
+    http.start
 
-    socket = connection.instance_variable_get :@socket
+    socket = http.instance_variable_get :@socket
 
     if socket then # for fakeweb
       @socket_options.each do |option|
@@ -758,13 +740,8 @@ class Net::HTTP::Persistent
   ##
   # Finishes the Net::HTTP +connection+
 
-  def finish connection, thread = Thread.current
-    if requests = thread[@request_key] then
-      requests.delete connection.object_id
-    end
-
+  def finish connection
     connection.finish
-  rescue IOError
   end
 
   def http_class # :nodoc:
@@ -869,9 +846,9 @@ class Net::HTTP::Persistent
   # <tt>net-http-persistent</tt> #pipeline will be present.
 
   def pipeline uri, requests, &block # :yields: responses
-    connection = connection_for uri
-
-    connection.pipeline requests, &block
+    connection_for uri do |connection|
+      connection.http.pipeline requests, &block
+    end
   end
 
   ##
@@ -1004,18 +981,17 @@ class Net::HTTP::Persistent
   # Finishes then restarts the Net::HTTP +connection+
 
   def reset connection
-    Thread.current[@request_key].delete connection.object_id
-    Thread.current[@timeout_key].delete connection.object_id
+    http = connection.http
 
     finish connection
 
-    start connection
+    start http
   rescue Errno::ECONNREFUSED
-    e = Error.new "connection refused: #{connection.address}:#{connection.port}"
+    e = Error.new "connection refused: #{http.address}:#{http.port}"
     e.set_backtrace $@
     raise e
   rescue Errno::EHOSTDOWN
-    e = Error.new "host down: #{connection.address}:#{connection.port}"
+    e = Error.new "host down: #{http.address}:#{http.port}"
     e.set_backtrace $@
     raise e
   end
@@ -1036,52 +1012,55 @@ class Net::HTTP::Persistent
     retried      = false
     bad_response = false
 
-    req = request_setup req || uri
+    req      = request_setup req || uri
+    response = nil
 
-    connection = connection_for uri
-    connection_id = connection.object_id
+    connection_for uri do |connection|
+      http = connection.http
 
-    begin
-      Thread.current[@request_key][connection_id] += 1
-      response = connection.request req, &block
+      begin
+        connection.requests += 1
 
-      if connection_close?(req) or
-         (response.http_version <= '1.0' and
-          not connection_keep_alive?(response)) or
-         connection_close?(response) then
-        connection.finish
-      end
-    rescue Net::HTTPBadResponse => e
-      message = error_message connection
+        response = http.request req, &block
 
-      finish connection
+        if connection_close?(req) or
+           (response.http_version <= '1.0' and
+            not connection_keep_alive?(response)) or
+           connection_close?(response) then
+          finish connection
+        end
+      rescue Net::HTTPBadResponse => e
+        message = error_message connection
 
-      raise Error, "too many bad responses #{message}" if
+        finish connection
+
+        raise Error, "too many bad responses #{message}" if
         bad_response or not can_retry? req
 
-      bad_response = true
-      retry
-    rescue *RETRIED_EXCEPTIONS => e # retried on ruby 2
-      request_failed e, req, connection if
-        retried or not can_retry? req, @retried_on_ruby_2
+        bad_response = true
+        retry
+      rescue *RETRIED_EXCEPTIONS => e # retried on ruby 2
+        request_failed e, req, connection if
+          retried or not can_retry? req, @retried_on_ruby_2
 
-      reset connection
+        reset connection
 
-      retried = true
-      retry
-    rescue Errno::EINVAL, Errno::ETIMEDOUT => e # not retried on ruby 2
-      request_failed e, req, connection if retried or not can_retry? req
+        retried = true
+        retry
+      rescue Errno::EINVAL, Errno::ETIMEDOUT => e # not retried on ruby 2
+        request_failed e, req, connection if retried or not can_retry? req
 
-      reset connection
+        reset connection
 
-      retried = true
-      retry
-    rescue Exception => e
-      finish connection
+        retried = true
+        retry
+      rescue Exception => e
+        finish connection
 
-      raise
-    ensure
-      Thread.current[@timeout_key][connection_id] = Time.now
+        raise
+      ensure
+        connection.last_use = Time.now
+      end
     end
 
     @http_versions["#{uri.host}:#{uri.port}"] ||= response.http_version
@@ -1100,7 +1079,6 @@ class Net::HTTP::Persistent
     message = "too many connection resets #{due_to} #{error_message connection}"
 
     finish connection
-
 
     raise Error, message, exception.backtrace
   end
@@ -1135,45 +1113,17 @@ class Net::HTTP::Persistent
   end
 
   ##
-  # Shuts down all connections for +thread+.
+  # Shuts down all connections
   #
-  # Uses the current thread by default.
+  # *NOTE*: Calling shutdown for can be dangerous!
   #
-  # If you've used Net::HTTP::Persistent across multiple threads you should
-  # call this in each thread when you're done making HTTP requests.
-  #
-  # *NOTE*: Calling shutdown for another thread can be dangerous!
-  #
-  # If the thread is still using the connection it may cause an error!  It is
-  # best to call #shutdown in the thread at the appropriate time instead!
+  # If any thread is still using a connection it may cause an error!  Call
+  # #shutdown when you are completely done making requests!
 
-  def shutdown thread = Thread.current
-    generation = reconnect
-    cleanup generation, thread, @generation_key
-
-    ssl_generation = reconnect_ssl
-    cleanup ssl_generation, thread, @ssl_generation_key
-
-    thread[@request_key] = nil
-    thread[@timeout_key] = nil
-  end
-
-  ##
-  # Shuts down all connections in all threads
-  #
-  # *NOTE*: THIS METHOD IS VERY DANGEROUS!
-  #
-  # Do not call this method if other threads are still using their
-  # connections!  Call #shutdown at the appropriate time instead!
-  #
-  # Use this method only as a last resort!
-
-  def shutdown_in_all_threads
-    Thread.list.each do |thread|
-      shutdown thread
+  def shutdown
+    @pool.available.shutdown do |http|
+      http.finish
     end
-
-    nil
   end
 
   ##
@@ -1239,14 +1189,6 @@ application:
   end
 
   ##
-  # Finishes all connections that existed before the given SSL parameter
-  # +generation+.
-
-  def ssl_cleanup generation # :nodoc:
-    cleanup generation, Thread.current, @ssl_generation_key
-  end
-
-  ##
   # SSL session lifetime
 
   def ssl_timeout= ssl_timeout
@@ -1297,5 +1239,7 @@ application:
 
 end
 
+require 'net/http/persistent/connection'
+require 'net/http/persistent/pool'
 require 'net/http/persistent/ssl_reuse'
 

lib/net/http/persistent/pool.rb
@@ -1,0 +1,46 @@
+class Net::HTTP::Persistent::Pool < ConnectionPool # :nodoc:
+
+  attr_reader :available # :nodoc:
+  attr_reader :key # :nodoc:
+
+  def initialize(options = {}, &block)
+    super
+
+    @available = Net::HTTP::Persistent::TimedStackMulti.new(@size, &block)
+    @key = :"current-#{@available.object_id}"
+  end
+
+  def checkin net_http_args
+    stack = Thread.current[@key][net_http_args]
+
+    raise ConnectionPool::Error, 'no connections are checked out' if
+      stack.empty?
+
+    conn = stack.pop
+
+    if stack.empty?
+      @available.push conn, connection_args: net_http_args
+    end
+
+    nil
+  end
+
+  def checkout net_http_args
+    stacks = Thread.current[@key] ||= Hash.new { |h, k| h[k] = [] }
+    stack  = stacks[net_http_args]
+
+    if stack.empty? then
+      conn = @available.pop connection_args: net_http_args
+    else
+      conn = stack.last
+    end
+
+    stack.push conn
+
+    conn
+  end
+
+end
+
+require 'net/http/persistent/timed_stack_multi'
+

Rakefile
@@ -18,7 +18,8 @@ Hoe.spec 'net-http-persistent' do
   rdoc_locations <<
     'docs.seattlerb.org:/data/www/docs.seattlerb.org/net-http-persistent/'
 
-  dependency 'minitest', '~> 5.2', :development
+  dependency 'connection_pool', '~> 2.1'
+  dependency 'minitest',        '~> 5.2', :development
 end
 
 # vim: syntax=Ruby

lib/net/http/persistent/connection.rb
@@ -1,0 +1,40 @@
+##
+# A Net::HTTP connection wrapper that holds extra information for managing the
+# connection's lifetime.
+
+class Net::HTTP::Persistent::Connection # :nodoc:
+
+  attr_accessor :http
+
+  attr_accessor :last_use
+
+  attr_accessor :requests
+
+  attr_accessor :ssl_generation
+
+  def initialize http_class, http_args, ssl_generation
+    @http           = http_class.new(*http_args)
+    @ssl_generation = ssl_generation
+
+    reset
+  end
+
+  def finish
+    @http.finish
+  rescue IOError
+  ensure
+    reset
+  end
+
+  def reset
+    @last_use = Net::HTTP::Persistent::EPOCH
+    @requests = 0
+  end
+
+  def ressl ssl_generation
+    @ssl_generation = ssl_generation
+
+    finish
+  end
+
+end

lib/net/http/persistent/timed_stack_multi.rb
@@ -1,0 +1,69 @@
+class Net::HTTP::Persistent::TimedStackMulti < ConnectionPool::TimedStack
+
+  def initialize(size = 0, &block)
+    super
+
+    @enqueued = 0
+    @ques = Hash.new { |h, k| h[k] = [] }
+    @lru = {}
+    @key = :"connection_args-#{object_id}"
+  end
+
+  def empty?
+    (@created - @enqueued) >= @max
+  end
+
+  def length
+    @max - @created + @enqueued
+  end
+
+  private
+
+  def connection_stored? options = {} # :nodoc:
+    !@ques[options[:connection_args]].empty?
+  end
+
+  def fetch_connection options = {} # :nodoc:
+    connection_args = options[:connection_args]
+
+    @enqueued -= 1
+    lru_update connection_args
+    @ques[connection_args].pop
+  end
+
+  def lru_update connection_args # :nodoc:
+    @lru.delete connection_args
+    @lru[connection_args] = true
+  end
+
+  def shutdown_connections # :nodoc:
+    @ques.each_key do |key|
+      super connection_args: key
+    end
+  end
+
+  def store_connection obj, options = {} # :nodoc:
+    @ques[options[:connection_args]].push obj
+    @enqueued += 1
+  end
+
+  def try_create options = {} # :nodoc:
+    connection_args = options[:connection_args]
+
+    if @created >= @max && @enqueued >= 1
+      oldest, = @lru.first
+      @lru.delete oldest
+      @ques[oldest].pop
+
+      @created -= 1
+    end
+
+    if @created < @max
+      @created += 1
+      lru_update connection_args
+      return @create_block.call(connection_args)
+    end
+  end
+
+end
+

test/test_net_http_persistent_timed_stack_multi.rb
@@ -1,0 +1,151 @@
+require 'minitest/autorun'
+require 'net/http/persistent'
+
+class TestNetHttpPersistentTimedStackMulti < Minitest::Test
+
+  class Connection
+    attr_reader :host
+
+    def initialize(host)
+      @host = host
+    end
+  end
+
+  def setup
+    @stack = Net::HTTP::Persistent::TimedStackMulti.new { Object.new }
+  end
+
+  def test_empty_eh
+    stack = Net::HTTP::Persistent::TimedStackMulti.new(1) { Object.new }
+
+    refute_empty stack
+
+    popped = stack.pop
+
+    assert_empty stack
+
+    stack.push connection_args: popped
+
+    refute_empty stack
+  end
+
+  def test_length
+    stack = Net::HTTP::Persistent::TimedStackMulti.new(1) { Object.new }
+
+    assert_equal 1, stack.length
+
+    popped = stack.pop
+
+    assert_equal 0, stack.length
+
+    stack.push connection_args: popped
+
+    assert_equal 1, stack.length
+  end
+
+  def test_pop
+    object = Object.new
+    @stack.push object
+
+    popped = @stack.pop
+
+    assert_same object, popped
+  end
+
+  def test_pop_empty
+    e = assert_raises Timeout::Error do
+      @stack.pop timeout: 0
+    end
+
+    assert_equal 'Waited 0 sec', e.message
+  end
+
+  def test_pop_full
+    stack = Net::HTTP::Persistent::TimedStackMulti.new(1) { Object.new }
+
+    popped = stack.pop
+
+    refute_nil popped
+    assert_empty stack
+  end
+
+  def test_pop_wait
+    thread = Thread.start do
+      @stack.pop
+    end
+
+    Thread.pass while thread.status == 'run'
+
+    object = Object.new
+
+    @stack.push object
+
+    assert_same object, thread.value
+  end
+
+  def test_pop_shutdown
+    @stack.shutdown { }
+
+    assert_raises ConnectionPool::PoolShuttingDownError do
+      @stack.pop
+    end
+  end
+
+  def test_push
+    stack = Net::HTTP::Persistent::TimedStackMulti.new(1) { Object.new }
+
+    conn = stack.pop
+
+    stack.push connection_args: conn
+
+    refute_empty stack
+  end
+
+  def test_push_shutdown
+    called = []
+
+    @stack.shutdown do |object|
+      called << object
+    end
+
+    @stack.push connection_args: Object.new
+
+    refute_empty called
+    assert_empty @stack
+  end
+
+  def test_shutdown
+    @stack.push connection_args: Object.new
+
+    called = []
+
+    @stack.shutdown do |object|
+      called << object
+    end
+
+    refute_empty called
+    assert_empty @stack
+  end
+
+  def test_pop_recycle
+    stack = Net::HTTP::Persistent::TimedStackMulti.new(2) { |host| Connection.new(host) }
+
+    a_conn = stack.pop connection_args: 'a.example'
+    stack.push a_conn, connection_args: 'a.example'
+
+    b_conn = stack.pop connection_args: 'b.example'
+    stack.push b_conn, connection_args: 'b.example'
+
+    c_conn = stack.pop connection_args: 'c.example'
+
+    assert_equal 'c.example', c_conn.host
+
+    stack.push c_conn, connection_args: 'c.example'
+
+    recreated = stack.pop connection_args: 'a.example'
+
+    refute_same a_conn, recreated
+  end
+
+end
+

commit 5be3611ff3540990013fb54d27482ccc344a557f

fix memory leak introduced by connection pooling
5bae22e8a3626ab3b14743c00796d4f176580ad8 introduced a memory leak when
it switched to connection pooling.  We're setting a thread local but
then never clearing the local.  If the pool is used in the main thread,
it will never die and the locals will stick around for the life of the
process.

Here is the program I used to test:

```
require 'net/http/persistent'

def run2
  1000.times do
    uri = URI 'http://localhost:5000'
    http = Net::HTTP::Persistent.new name: 'my_app_name'
    # perform a GET
    response = http.request uri
    # if you are done making http requests, or won't make requests for several
    # minutes
    http.shutdown
  end
end

p Thread.current.keys
run2
p Thread.current.keys
```

lib/net/http/persistent.rb
@@ -1048,9 +1048,7 @@ class Net::HTTP::Persistent
   # #shutdown when you are completely done making requests!
 
   def shutdown
-    @pool.available.shutdown do |http|
-      http.finish
-    end
+    @pool.shutdown { |http| http.finish }
   end
 
   ##

lib/net/http/persistent/pool.rb
@@ -7,11 +7,11 @@ class Net::HTTP::Persistent::Pool < ConnectionPool # :nodoc:
     super
 
     @available = Net::HTTP::Persistent::TimedStackMulti.new(@size, &block)
-    @key = :"current-#{@available.object_id}"
+    @key = "current-#{@available.object_id}"
   end
 
   def checkin net_http_args
-    stack = Thread.current[@key][net_http_args]
+    stack = Thread.current[@key][net_http_args] ||= []
 
     raise ConnectionPool::Error, 'no connections are checked out' if
       stack.empty?
@@ -26,8 +26,8 @@ class Net::HTTP::Persistent::Pool < ConnectionPool # :nodoc:
   end
 
   def checkout net_http_args
-    stacks = Thread.current[@key] ||= Hash.new { |h, k| h[k] = [] }
-    stack  = stacks[net_http_args]
+    stacks = Thread.current[@key] ||= {}
+    stack  = stacks[net_http_args] ||= []
 
     if stack.empty? then
       conn = @available.pop connection_args: net_http_args
@@ -40,6 +40,10 @@ class Net::HTTP::Persistent::Pool < ConnectionPool # :nodoc:
     conn
   end
 
+  def shutdown
+    Thread.current[@key] = nil
+    super
+  end
 end
 
 require 'net/http/persistent/timed_stack_multi'