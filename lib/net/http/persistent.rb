ca8e0929135b2462b346902e372a2f75c29228d1
Eric Hodel <drbrain@segment7.net>
2019-07-24 21:21:44 -0700

    Update VERSION and History.txt for 3.1.0

--- lib/net/http/persistent.rb
+++ lib/net/http/persistent.rb
@@ -211,7 +211,7 @@ class Net::HTTP::Persistent
   ##
   # The version of Net::HTTP::Persistent you are using
 
-  VERSION = '3.0.0'
+  VERSION = '3.1.0'
 
   ##
   # Exceptions rescued for automatic retry on ruby 2.0.0.  This overlaps with
