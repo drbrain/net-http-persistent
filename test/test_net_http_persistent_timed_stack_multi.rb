bd43e18732c32ff894214daecb98ac35b2814988
Aaron Patterson <tenderlove@ruby-lang.org>
2023-03-29 11:56:22 -0700

    Use `assert_match`
    This should match the message regardless of version

--- test/test_net_http_persistent_timed_stack_multi.rb
+++ test/test_net_http_persistent_timed_stack_multi.rb
@@ -57,7 +57,7 @@ class TestNetHttpPersistentTimedStackMulti < Minitest::Test
       @stack.pop timeout: 0
     end
 
-    assert_equal 'Waited 0 sec, 0/0 available', e.message
+    assert_match 'Waited 0 sec', e.message
   end
 
   def test_pop_full
